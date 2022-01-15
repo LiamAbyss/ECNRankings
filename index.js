97b74bc8f9adf88d972ea7b57d79a823bb59ea9c
LiamAbyss <46683039+LiamAbyss@users.noreply.github.com>
2022-01-15 18:35:40 +0100

    Add port for heroku

--- index.js
+++ index.js
@@ -179,7 +179,6 @@ app.use(function (req, res, next) {
 })
 
 // run server
-app.listen(3000, () => {
-    console.log('server running on port 3000')
-    }
-)
+app.listen(process.env.PORT, () => {
+    console.log(`server running on port ${process.env.PORT}`)
+})
